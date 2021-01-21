476385624d9fcbb5d8e6f99ee4b3f74fa72a2940
Ник Николос <nic23172@gmail.com>
2021-01-21 11:23:47 +0300

    initial

---
+++ animate.js
@@ -0,0 +1,14 @@
+(function(){
+	const animateButton = function(e) {
+  e.preventDefault;
+  e.target.classList.remove('animate');
+    e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+const bubblyButtons = document.getElementsByClassName("button");
+for (let i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+})()
---
+++ main.js
@@ -0,0 +1,66 @@
+ninjaDataOne =  {
+  name: 'Naruto',
+  type: 'unknown',
+  hp: 564,
+  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014924/naruto_jrwlvd.png',
+  attacks: [
+      {
+          name: "Clone Lasso",
+          maxDamage: 40,
+          minDamage: 20,
+          maxCount: 100,
+      },
+      {
+          name: "Rasengan",
+          maxDamage: 60,
+          minDamage: 45,
+          maxCount: 6,
+      },
+      {
+          name: "Rasenshuriken",
+          maxDamage: 75,
+          minDamage: 60,
+          maxCount: 4,
+      },
+      {
+          name: "Twin Rasenshuriken",
+          maxDamage: 160,
+          minDamage: 130,
+          maxCount: 2,
+      }
+  ]
+}
+
+nanjaDataTwo = {
+  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/gaara_pnzqeo.png',
+  name: 'Gaara',
+  type: 'unknown',
+  hp: 282,
+  attacks: [
+      {
+          name: "Sand Shield",
+          maxDamage: 40,
+          minDamage: 20,
+          maxCount: 100,
+      },
+      {
+          name: "Sand Swords",
+          maxDamage: 60,
+          minDamage: 45,
+          maxCount: 6,
+      },
+      {
+          name: "Sand Shuriken",
+          maxDamage: 75,
+          minDamage: 60,
+          maxCount: 4,
+      },
+      {
+          name: "Sand Clone",
+          maxDamage: 130,
+          minDamage: 110,
+          maxCount: 2,
+      }
+  ]
+}
+
